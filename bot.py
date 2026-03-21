c086130671adf52a25f695f6559538ae8c2a5c16
SHAIKH SAHIL ALI <101174563+sksahil03@users.noreply.github.com>
2026-03-21 15:37:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,7 +43,8 @@ BOT_TOKEN = "8683877270:AAE92_iTUBdNst5TVPCGaKseajql4buOURo"
 # Example: ADMINS = ["Sahil_sciastra", "newadmin123"]
 ADMINS = [
     "Sahil_sciastra",    # Main admin — tagged by /guide
-    "Evyavansachan",     # Remove the # at the start to activate
+    "Evyavansachan",
+    "jester_here", # Remove the # at the start to activate
 ]
 
 # Main admin is always the first one in the list
